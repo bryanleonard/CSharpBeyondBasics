7f204730e76e2ee108f22b774bcc49aa20dd8a89
bryanleonard <bleonard@acrtinc.com>
2018-10-03 17:04:32 -0400

    Adding some Product and tests.

---
+++ Acme.Biz/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products carried in inventory
+    /// </summary>
+    public class Product
+    {
+        public Product()
+        {
+            Console.WriteLine("Product instance created");
+        }
+
+        public Product(int productId, string productName, string description) : this()
+        {
+            Console.WriteLine("Not the same code");
+            this.ProductId = productId;
+            this.ProductName = productName;
+            this.Description = description;
+        }
+
+        // ~ 6:15 min in on Defining Constructors
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+
+        public string SayHello()
+        {
+            return $"Hello ({ProductId}) {ProductName}: {Description}";
+        }
+    }
+}
---
+++ Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Acme.Biz;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Acme.BizTests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void SayHelloTest()
+        {
+            //Arrange
+            var currentProduct = new Product
+            {
+                ProductId = 1,
+                ProductName = "Saw",
+                Description = "15-inch steel blade hand saw"
+            };
+
+            //Act
+            var actual = currentProduct.SayHello();
+            var expected = "Hello (1) Saw: 15-inch steel blade hand saw";
+
+            //Asset
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SayHello_ParameterizedConstructor()
+        {
+            //Arrange
+            var currentProduct = new Product(1, "Saw", "15-inch steel blade hand saw");
+
+            //Act
+            var actual = currentProduct.SayHello();
+            var expected = "Hello (1) Saw: 15-inch steel blade hand saw";
+
+            //Asset
+            Assert.Equal(expected, actual);
+        }
+    }
+}
